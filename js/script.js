73099e0e92219a8f2779418249eff8dcf10ba0d4
fadeev <fadeevomck@mail.ru>
2017-04-21 18:52:53 +0600

    Первый коммент

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//alert('Hi');-Мы не будем выводитьсщщбщение
+var number = 3;
+var myString = 'Hi world!';
+console.log(number);
+console.log('number');
+console.log(myString);
+
+var numberTwo = 2;
+number = number * numberTwo;
+
+console.log(number);
+
+$(document).ready(function(){
+$('.colorbox').colorbox({
+
+});
+$('.flexslider').flexslider({
+        animation:"slide",
+        slideshowSpeed:"16000"
+    });
+});
